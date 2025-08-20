780106950e05fe32cbf1823e6778f88b126ddbc9
DoubleSpirit <doublespirit@DoubleSpiritdeMac-mini.local>
2025-08-20 22:47:40 +0800

    修复BUG

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,41 +1,17 @@
-// .vitepress/theme/index.js
+// https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
-import busuanzi from 'busuanzi.pure.js'
 
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
-  Layout() {
-    // 在页面底部插入计数器
+  Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      'layout-bottom': () =>
-        h(
-          'div',
-          {
-            style: {
-              textAlign: 'center',
-              fontSize: '0.85em',
-              color: '#666',
-              margin: '2rem 0'
-            }
-          },
-          [
-            '🤯 本站累计访问量 ',
-            h('span', { id: 'busuanzi_value_site_pv' }),
-            ' 次，😱 累计访客数 ',
-            h('span', { id: 'busuanzi_value_site_uv' }),
-            ' 人次'
-          ]
-        )
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
-  enhanceApp({ router }) {
-    // 浏览器环境才执行
-    if (typeof window !== 'undefined') {
-      busuanzi.fetch()                      // 首次加载
-      router.onAfterRouteChanged = () => busuanzi.fetch() // 路由切换后重新统计
-    }
+  enhanceApp({ app, router, siteData }) {
+    // ...
   }
 }
